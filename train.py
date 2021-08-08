ea0cfa14a628bae8414141cf53a6e89da41d128d
alirezaabdollahpour <you@example.com>
2021-08-09 00:19:37 +0430

    attenion visualize function delete

--- train.py
+++ train.py
@@ -65,50 +65,7 @@ def save_model(args, model):
     logger.info("Saved model checkpoint to [DIR: %s]", args.output_dir)
 
 
-""""
-function for show image and adversarial image--> we hava an error in assignment to model!
-def get_attention_map(img, get_mask=False):
-    x = transform(img)
-    x.size()
 
-    logits, att_mat = model(x.unsqueeze(0))
-
-    att_mat = torch.stack(att_mat).squeeze(1)
-
-    # Average the attention weights across all heads.
-    att_mat = torch.mean(att_mat, dim=1)
-
-    # To account for residual connections, we add an identity matrix to the
-    # attention matrix and re-normalize the weights.
-    residual_att = torch.eye(att_mat.size(1))
-    aug_att_mat = att_mat + residual_att
-    aug_att_mat = aug_att_mat / aug_att_mat.sum(dim=-1).unsqueeze(-1)
-
-    # Recursively multiply the weight matrices
-    joint_attentions = torch.zeros(aug_att_mat.size())
-    joint_attentions[0] = aug_att_mat[0]
-
-    for n in range(1, aug_att_mat.size(0)):
-        joint_attentions[n] = torch.matmul(aug_att_mat[n], joint_attentions[n-1])
-
-    v = joint_attentions[-1]
-    grid_size = int(np.sqrt(aug_att_mat.size(-1)))
-    mask = v[0, 1:].reshape(grid_size, grid_size).detach().numpy()
-    if get_mask:
-        result = cv2.resize(mask / mask.max(), img.size)
-    else:        
-        mask = cv2.resize(mask / mask.max(), img.size)[..., np.newaxis]
-        result = (mask * img).astype("uint8")
-
-    return result
-
-def plot_attention_map(original_img, att_map):
-    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(16, 16))
-    ax1.set_title('Original')
-    ax2.set_title('Attention Map Last Layer')
-    _ = ax1.imshow(original_img)
-    _ = ax2.imshow(att_map)
-"""
 
 
 def setup(args):
