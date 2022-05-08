2e40e8eab7171c5c18398f89daeaf6294f2bb285
AlifAshrafee <alifjpt@gmail.com>
2022-05-08 14:50:23 +0600

    Confusion matrix and classification report added, valid loss scalar added, loading pre-trained weights again

--- train.py
+++ train.py
@@ -22,6 +22,11 @@ from utils.scheduler import WarmupLinearSchedule, WarmupCosineSchedule
 from utils.data_utils import get_loader
 from utils.dist_util import get_world_size
 
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import matplotlib.pyplot as plt
+
 
 logger = logging.getLogger(__name__)
 
@@ -63,7 +68,7 @@ def setup(args):
     num_classes = 10 if args.dataset == "cifar10" else 7
 
     model = VisionTransformer(config, args.img_size, zero_head=True, num_classes=num_classes)
-    # model.load_from(np.load(args.pretrained_dir))
+    model.load_from(np.load(args.pretrained_dir))
     model.to(args.device)
     num_params = count_parameters(model)
 
@@ -147,6 +152,8 @@ def valid(args, model, writer, valid_loader, global_step):
     logger.info("Valid Accuracy: %2.5f" % accuracy)
 
     writer.add_scalar("valid/accuracy", scalar_value=accuracy, global_step=global_step)
+    writer.add_scalar("valid/loss", scalar_value=eval_losses.val, global_step=global_step)
+
     return accuracy
 
 def test(args, model, writer, test_loader, global_step):
@@ -204,6 +211,17 @@ def test(args, model, writer, test_loader, global_step):
     logger.info("Global Steps: %d" % global_step)
     logger.info("Test Loss: %2.5f" % eval_losses.avg)
     logger.info("Test Accuracy: %2.5f" % accuracy)
+
+    target_names = ['creamy_paste', 'diced', 'grated', 'juiced', 'jullienne', 'sliced', 'whole']
+
+    print("\n")
+    print("Classification Report")
+    print(classification_report(all_label, all_preds, target_names=target_names, digits=2))
+
+    cm = confusion_matrix(all_label, all_preds)
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    plt.figure(figsize = (12, 8))
+    sn.heatmap(cm, annot=True)
 
     writer.add_scalar("test/accuracy", scalar_value=accuracy, global_step=global_step)
     return accuracy
